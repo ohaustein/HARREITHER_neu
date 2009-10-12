5a321b5d1d4db2c04c21c63ccc8df1a4213372b5
neudorfer <neudorfer@ada20c56-b04a-594b-83cb-56bd2b467f42>
2009-10-12 12:55:15 +0000

    tabs für produkte

--- trunk/src/europlan2.0/Common/Project/SystemParametersPanel.Designer.cs
+++ trunk/src/europlan2.0/Common/Project/SystemParametersPanel.Designer.cs
@@ -24,6 +24,12 @@ namespace Europlan.Common {
 		/// </summary>
 		private void InitializeComponent() {
 			this.label1 = new System.Windows.Forms.Label();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabEuroval = new System.Windows.Forms.TabPage();
+			this.tabHitherm = new System.Windows.Forms.TabPage();
+			this.tabModulBoden = new System.Windows.Forms.TabPage();
+			this.tabModulDecke = new System.Windows.Forms.TabPage();
+			this.tabControl1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -36,13 +42,67 @@ namespace Europlan.Common {
 			this.label1.TabIndex = 3;
 			this.label1.Text = "Systemparameter";
 			// 
+			// tabControl1
+			// 
+			this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.tabControl1.Controls.Add(this.tabEuroval);
+			this.tabControl1.Controls.Add(this.tabHitherm);
+			this.tabControl1.Controls.Add(this.tabModulBoden);
+			this.tabControl1.Controls.Add(this.tabModulDecke);
+			this.tabControl1.Location = new System.Drawing.Point(0, 27);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(719, 392);
+			this.tabControl1.TabIndex = 4;
+			// 
+			// tabEuroval
+			// 
+			this.tabEuroval.Location = new System.Drawing.Point(4, 22);
+			this.tabEuroval.Name = "tabEuroval";
+			this.tabEuroval.Padding = new System.Windows.Forms.Padding(3);
+			this.tabEuroval.Size = new System.Drawing.Size(711, 366);
+			this.tabEuroval.TabIndex = 0;
+			this.tabEuroval.Text = "Euroval®";
+			this.tabEuroval.UseVisualStyleBackColor = true;
+			// 
+			// tabHitherm
+			// 
+			this.tabHitherm.Location = new System.Drawing.Point(4, 22);
+			this.tabHitherm.Name = "tabHitherm";
+			this.tabHitherm.Size = new System.Drawing.Size(711, 366);
+			this.tabHitherm.TabIndex = 1;
+			this.tabHitherm.Text = "Hitherm®";
+			this.tabHitherm.UseVisualStyleBackColor = true;
+			// 
+			// tabModulBoden
+			// 
+			this.tabModulBoden.Location = new System.Drawing.Point(4, 22);
+			this.tabModulBoden.Name = "tabModulBoden";
+			this.tabModulBoden.Size = new System.Drawing.Size(711, 366);
+			this.tabModulBoden.TabIndex = 2;
+			this.tabModulBoden.Text = "Modul Klima-Boden";
+			this.tabModulBoden.UseVisualStyleBackColor = true;
+			// 
+			// tabModulDecke
+			// 
+			this.tabModulDecke.Location = new System.Drawing.Point(4, 22);
+			this.tabModulDecke.Name = "tabModulDecke";
+			this.tabModulDecke.Size = new System.Drawing.Size(711, 366);
+			this.tabModulDecke.TabIndex = 3;
+			this.tabModulDecke.Text = "Modul Klima-Decke";
+			this.tabModulDecke.UseVisualStyleBackColor = true;
+			// 
 			// SystemParametersPanel
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.tabControl1);
 			this.Controls.Add(this.label1);
 			this.Name = "SystemParametersPanel";
 			this.Size = new System.Drawing.Size(719, 422);
+			this.tabControl1.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -51,5 +111,10 @@ namespace Europlan.Common {
 		#endregion
 
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabEuroval;
+		private System.Windows.Forms.TabPage tabHitherm;
+		private System.Windows.Forms.TabPage tabModulBoden;
+		private System.Windows.Forms.TabPage tabModulDecke;
 	}
 }
