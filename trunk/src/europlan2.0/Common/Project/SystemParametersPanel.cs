e0488718f27abcef6599c134363d3f2e15908193
neudorfer <neudorfer@ada20c56-b04a-594b-83cb-56bd2b467f42>
2009-10-13 08:48:41 +0000

    systemparameter für euroval

--- trunk/src/europlan2.0/Common/Project/SystemParametersPanel.cs
+++ trunk/src/europlan2.0/Common/Project/SystemParametersPanel.cs
@@ -16,6 +16,7 @@ namespace Europlan.Common {
 		
 		public SystemParametersPanel() {
 			InitializeComponent();
+			InitializeEurovalValues();
 		}
 
 		public void UpdateControl() {
@@ -24,6 +25,71 @@ namespace Europlan.Common {
 
 		public bool AllowLeave() {
 			return true;
+		}
+
+		private void btnEurovalStandard_Click(object sender, EventArgs e) {
+			InitializeEurovalValues();
+		}
+
+		private void InitializeEurovalValues() {
+			rbHarreitherNorm.Checked = true;
+			numCircuitLength.Value = 100;
+			numPressurePa.Value = 15000;
+			numDurchfluss.Value = 240;
+			numSpreizungHeizMin.Value = 4;
+			numSpreizungHeizMax.Value = 12;
+			numSpreizungKühlMin.Value = 2;
+			numSpreizungKühlMax.Value = 6;
+		}
+
+		private void numCircuitLength_ValueChanged(object sender, EventArgs e) {
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+		private void numDurchfluss_ValueChanged(object sender, EventArgs e) {
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+		private void numPressurePa_ValueChanged(object sender, EventArgs e) {
+			numPressureMbar.Value = numPressurePa.Value / 100;
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+		private void numPressureMbar_ValueChanged(object sender, EventArgs e) {
+			numPressurePa.Value = numPressureMbar.Value * 100;
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+		private void numSpreizungHeizMin_ValueChanged(object sender, EventArgs e) {
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+		private void numSpreizungHeizMax_ValueChanged(object sender, EventArgs e) {
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+		private void numSpreizungKühlMin_ValueChanged(object sender, EventArgs e) {
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+		private void numSpreizungKühlMax_ValueChanged(object sender, EventArgs e) {
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
 		}
 
 	}
