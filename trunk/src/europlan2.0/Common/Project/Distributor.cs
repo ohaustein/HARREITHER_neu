82fecfbfd25378065820e58f0bef1bbcef38cc6a
neudorfer <neudorfer@ada20c56-b04a-594b-83cb-56bd2b467f42>
2009-10-22 12:03:43 +0000

    vervollständigen von gui für verteiler

--- trunk/src/europlan2.0/Common/Project/Distributor.cs
+++ trunk/src/europlan2.0/Common/Project/Distributor.cs
@@ -10,13 +10,130 @@ namespace Europlan.Common {
 	[Serializable()]
 	public class Distributor : IGuiRepresentation {
 
+#region enums
+
+		public class DistributorTypeEnumConverter : System.ComponentModel.TypeConverter {
+	
+			private static readonly string durchflussmengenregler = "Harreither Systemverteiler mit Durchflußmengenregler";
+			private static readonly string ruecklaufventil = "Harreither Systemverteiler mit Rücklaufventil";
+
+			private Dictionary<string, DistributorTypeEnum> mappingFromString = new Dictionary<string, DistributorTypeEnum>();
+			private Dictionary<DistributorTypeEnum, string> mappingToString = new Dictionary<DistributorTypeEnum, string>();
+
+			public DistributorTypeEnumConverter() {
+				mappingFromString.Add(durchflussmengenregler, DistributorTypeEnum.Durchflussmengenregler);
+				mappingFromString.Add(ruecklaufventil, DistributorTypeEnum.Ruecklaufventil);
+				mappingToString.Add(DistributorTypeEnum.Durchflussmengenregler, durchflussmengenregler);
+				mappingToString.Add(DistributorTypeEnum.Ruecklaufventil, ruecklaufventil);
+			}
+
+			public override bool CanConvertFrom(System.ComponentModel.ITypeDescriptorContext context, Type sourceType) {
+				return sourceType == typeof(string);
+			}
+
+			public override bool CanConvertTo(System.ComponentModel.ITypeDescriptorContext context, Type destinationType) {
+				return destinationType == typeof(string);
+			}
+
+			public override object ConvertFrom(System.ComponentModel.ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value) {
+				if (value is string) {
+					if (mappingFromString.ContainsKey((string)value)) {
+						return mappingFromString[(string)value];
+					}
+				}
+				return base.ConvertFrom(context, culture, value);
+			}
+
+			public override object ConvertTo(System.ComponentModel.ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType) {
+				if (value is DistributorTypeEnum && destinationType == typeof(string)) {
+					if (mappingToString.ContainsKey((DistributorTypeEnum)value)) {
+						return mappingToString[(DistributorTypeEnum)value];
+					}
+				}
+				return base.ConvertTo(context, culture, value, destinationType);
+			}
+		}
+
+		[System.ComponentModel.TypeConverter(typeof(DistributorTypeEnumConverter))]
+		public enum DistributorTypeEnum {
+			Durchflussmengenregler,
+			Ruecklaufventil
+		}
+
+		public class AnschlussHollaenderEnumConverter : System.ComponentModel.TypeConverter {
+
+			private static readonly string kein = "Kein Holländer";
+			private static readonly string hollaender32 = "Anschlußholländer mit Anschlußstück 32mm";
+			private static readonly string hollaenderIG = "Anschlußholländer mit Anschlußstück 1\" IG";
+			private static readonly string hollaenderAG = "Anschlußholländer mit Anschlußstück 1\" AG";
+
+			private Dictionary<string,  AnschlussHollaenderEnum> mappingFromString = new Dictionary<string,  AnschlussHollaenderEnum>();
+			private Dictionary< AnschlussHollaenderEnum, string> mappingToString = new Dictionary< AnschlussHollaenderEnum, string>();
+
+			public AnschlussHollaenderEnumConverter() {
+				mappingFromString.Add(kein, AnschlussHollaenderEnum.Kein);
+				mappingFromString.Add(hollaender32, AnschlussHollaenderEnum.hollaender32);
+				mappingFromString.Add(hollaenderIG, AnschlussHollaenderEnum.hollaenderIG);
+				mappingFromString.Add(hollaenderAG, AnschlussHollaenderEnum.hollaenderAG);
+				mappingToString.Add(AnschlussHollaenderEnum.Kein, kein);
+				mappingToString.Add(AnschlussHollaenderEnum.hollaender32, hollaender32);
+				mappingToString.Add(AnschlussHollaenderEnum.hollaenderIG, hollaenderIG);
+				mappingToString.Add(AnschlussHollaenderEnum.hollaenderAG, hollaenderAG);
+			}
+
+			public override bool CanConvertFrom(System.ComponentModel.ITypeDescriptorContext context, Type sourceType) {
+				return sourceType == typeof(string);
+			}
+
+			public override bool CanConvertTo(System.ComponentModel.ITypeDescriptorContext context, Type destinationType) {
+				return destinationType == typeof(string);
+			}
+
+			public override object ConvertFrom(System.ComponentModel.ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value) {
+				if (value is string) {
+					if (mappingFromString.ContainsKey((string)value)) {
+						return mappingFromString[(string)value];
+					}
+				}
+				return base.ConvertFrom(context, culture, value);
+			}
+
+			public override object ConvertTo(System.ComponentModel.ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType) {
+				if (value is  AnschlussHollaenderEnum && destinationType == typeof(string)) {
+					if (mappingToString.ContainsKey(( AnschlussHollaenderEnum)value)) {
+						return mappingToString[( AnschlussHollaenderEnum)value];
+					}
+				}
+				return base.ConvertTo(context, culture, value, destinationType);
+			}
+		}
+
+		[System.ComponentModel.TypeConverter(typeof(AnschlussHollaenderEnumConverter))]
+		public enum AnschlussHollaenderEnum {
+			Kein,
+			hollaender32,
+			hollaenderIG,
+			hollaenderAG
+		}
+
+#endregion
+
 		private string id;
 		private string name;
 		private string regulatorCircuitId = null;
 		private RegulatorCircuit regulatorCircuit = null;
+		private DistributorTypeEnum distributorType;
+		private AnschlussHollaenderEnum anschlussHollaender;
+		private bool langeAnschlussboegen;
 		private int maxCircuits;
+		private int additionalCircuits;
+		private int zusaetzlicheStellantriebe;
 		private bool flanschKugelHaehne;
 		private bool einbauSchrank;
+		private bool useForFloor;
+		private bool useForWall;
+		private bool useForCeiling;
+
 		private List<string> additionalFloors;
 
 		[NonSerialized]
@@ -36,11 +153,19 @@ namespace Europlan.Common {
 			id = "";
 			name = "";
 			regulatorCircuitId = "";
+			distributorType = DistributorTypeEnum.Durchflussmengenregler;
+			anschlussHollaender = AnschlussHollaenderEnum.Kein;
+			langeAnschlussboegen = false;
 			maxCircuits = 12;
+			additionalCircuits = 0;
+			zusaetzlicheStellantriebe = 0;
 			flanschKugelHaehne = true;
 			einbauSchrank = true;
 			additionalFloors = new List<string>();
 			distributorNode.Tag = this;
+			useForFloor = true;
+			useForWall = true;
+			useForCeiling = true;
 		}
 
 		public string Id {
@@ -61,6 +186,16 @@ namespace Europlan.Common {
 					distributorNode.Text = (String.IsNullOrEmpty(name) ? "unbenannt" : id + ": " + name);
 				}
 			}
+		}
+
+		public DistributorTypeEnum DistributorType {
+			get { return distributorType; }
+			set { distributorType = value; }
+		}
+
+		public AnschlussHollaenderEnum AnschlussHollaender {
+			get { return anschlussHollaender; }
+			set { anschlussHollaender = value; }
 		}
 
 		[XmlIgnore]
@@ -91,6 +226,16 @@ namespace Europlan.Common {
 			set { maxCircuits = value; }
 		}
 
+		public int AdditionalCircuits {
+			get { return additionalCircuits; }
+			set { additionalCircuits = value; }
+		}
+
+		public int ZusaetzlicheStellantriebe {
+			get { return zusaetzlicheStellantriebe; }
+			set { zusaetzlicheStellantriebe = value; }
+		}
+
 		public bool FlanschKugelHaehne {
 			get { return flanschKugelHaehne; }
 			set { flanschKugelHaehne = value; }
@@ -99,6 +244,26 @@ namespace Europlan.Common {
 		public bool EinbauSchrank {
 			get { return einbauSchrank; }
 			set { einbauSchrank = value; }
+		}
+
+		public bool LangeAnschlussboegen {
+			get { return langeAnschlussboegen; }
+			set { langeAnschlussboegen = value; }
+		}
+
+		public bool UseForFloor {
+			get { return useForFloor; }
+			set { useForFloor = value; }
+		}
+
+		public bool UseForWall {
+			get { return useForWall; }
+			set { useForWall = value; }
+		}
+
+		public bool UseForCeiling {
+			get { return useForCeiling; }
+			set { useForCeiling = value; }
 		}
 
 		internal TreeNode Node {
--- trunk/src/europlan2.0/Common/Project/DistributorPanel.cs
+++ trunk/src/europlan2.0/Common/Project/DistributorPanel.cs
@@ -25,12 +25,23 @@ namespace Europlan.Common {
 				this.lblId.Text = distributor.Id;
 				this.txtName.Text = distributor.Name;
 				this.numMaxCircuits.Value = distributor.MaxCircuits;
+				this.numAdditionalCircuits.Value = distributor.AdditionalCircuits;
+				this.numZusStellantriebe.Value = distributor.ZusaetzlicheStellantriebe;
 				this.chkFlansch.Checked = distributor.FlanschKugelHaehne;
 				this.chkEinbauschrank.Checked = distributor.EinbauSchrank;
+				this.chkAnschluss.Checked = distributor.LangeAnschlussboegen;
 				Project project = Project.Instance;
 				this.cmbCircuit.Items.Clear();
 				foreach (RegulatorCircuit circuit in project.RegulatorCircuits) {
 					this.cmbCircuit.Items.Add(circuit);
+				}
+				this.cmbDistributorType.Items.Clear();
+				foreach (Distributor.DistributorTypeEnum item in Enum.GetValues(typeof(Distributor.DistributorTypeEnum))) {
+					this.cmbDistributorType.Items.Add(item);
+				}
+				this.cmbAnschlussHollaender.Items.Clear();
+				foreach (Distributor.AnschlussHollaenderEnum item in Enum.GetValues(typeof(Distributor.AnschlussHollaenderEnum))) {
+					this.cmbAnschlussHollaender.Items.Add(item);
 				}
 				this.listFloors.Items.Clear();
 				foreach (Floor floor in project.Floors) {
@@ -38,7 +49,13 @@ namespace Europlan.Common {
 						this.listFloors.Items.Add(floor, this.distributor.AdditionalFloorIds.Contains(floor.Id));						
 					}
 				}
+				this.lstSystems.Items.Clear();
+				this.lstSystems.Items.Add("Fuﬂboden", distributor.UseForFloor);
+				this.lstSystems.Items.Add("Wand", distributor.UseForWall);
+				this.lstSystems.Items.Add("Decke", distributor.UseForCeiling);
 				this.cmbCircuit.SelectedItem = distributor.RegulatorCircuit;
+				this.cmbDistributorType.SelectedItem = distributor.DistributorType;
+				this.cmbAnschlussHollaender.SelectedItem = distributor.AnschlussHollaender;
 			}
 		}
 
@@ -62,6 +79,15 @@ namespace Europlan.Common {
 
 		private void numMaxCircuits_ValueChanged(object sender, EventArgs e) {
 			distributor.MaxCircuits = (int)this.numMaxCircuits.Value;
+			numAdditionalCircuits.Maximum = distributor.MaxCircuits;
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+
+		private void numAdditionalCircuits_ValueChanged(object sender, EventArgs e) {
+			distributor.AdditionalCircuits = (int)this.numAdditionalCircuits.Value;
 			if (ProjectChanged != null) {
 				ProjectChanged(null);
 			}
@@ -84,15 +110,60 @@ namespace Europlan.Common {
 			}
 		}
 
-		private void numFlanschkugelhaehne_ValueChanged(object sender, EventArgs e) {
+		private void chkEinbauschrank_CheckedChanged(object sender, EventArgs e) {
+			distributor.EinbauSchrank = this.chkEinbauschrank.Checked;
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+		private void cmbDistributorType_SelectedIndexChanged(object sender, EventArgs e) {
+			distributor.DistributorType = (Distributor.DistributorTypeEnum)this.cmbDistributorType.SelectedItem;
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+		private void cmbAnschlussHollaender_SelectedIndexChanged(object sender, EventArgs e) {
+			distributor.AnschlussHollaender = (Distributor.AnschlussHollaenderEnum)this.cmbAnschlussHollaender.SelectedItem;
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+		private void chkFlansch_CheckedChanged(object sender, EventArgs e) {
 			distributor.FlanschKugelHaehne = this.chkFlansch.Checked;
 			if (ProjectChanged != null) {
 				ProjectChanged(null);
 			}
 		}
 
-		private void chkEinbauschrank_CheckedChanged(object sender, EventArgs e) {
-			distributor.EinbauSchrank = this.chkEinbauschrank.Checked;
+		private void chkAnschluss_CheckedChanged(object sender, EventArgs e) {
+			distributor.LangeAnschlussboegen = this.chkAnschluss.Checked;
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+		private void numZusStellantriebe_ValueChanged(object sender, EventArgs e) {
+			distributor.ZusaetzlicheStellantriebe = (int)this.numZusStellantriebe.Value;
+			if (ProjectChanged != null) {
+				ProjectChanged(null);
+			}
+		}
+
+		private void lstSystems_ItemCheck(object sender, ItemCheckEventArgs e) {
+			switch (e.Index) {
+				case 0:
+					distributor.UseForFloor = e.NewValue == CheckState.Checked;
+					break;
+				case 1:
+					distributor.UseForWall = e.NewValue == CheckState.Checked;
+					break;
+				case 2:
+					distributor.UseForCeiling = e.NewValue == CheckState.Checked;
+					break;
+			}
 			if (ProjectChanged != null) {
 				ProjectChanged(null);
 			}
