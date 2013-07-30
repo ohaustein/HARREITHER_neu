30722633b68197c992881d73f3c466779df33174
winkler <winkler@ada20c56-b04a-594b-83cb-56bd2b467f42>
2013-07-30 16:28:20 +0000

    - modified copy_new_versions script to download setup files from jenkins instead of copying it from cruisecontrol

--- src/europlan2.0/copy_new_versions.ps1
+++ src/europlan2.0/copy_new_versions.ps1
@@ -1,15 +1,30 @@
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 
+$jenkinsUrl = "http://jenkins:8080/"
+$jenkinsJob = "europlan_grafisch_trunk"
+$versionRootDir = "\\fsh\Daten\Share\Software\europlan\versions_ga\"
+
 $version = Get-Content .\curversion.txt
-$versionDir = "\\eos\Share\Software\europlan\versions_ga\v" + $version
+$versionDir = $versionRootDir + "\v" + $version
+
 if (! ([IO.Directory]::Exists($versionDir))) {
+	$jenkinsUrlMsiDe = $jenkinsUrl + "view/Alle/job/" + $jenkinsJob + "/lastSuccessfulBuild/artifact/src/europlan2.0/Setup/bin/Release/de-DE/europlan30.msi"
+	$jenkinsUrlMsiEn = $jenkinsUrl + "view/Alle/job/" + $jenkinsJob + "/lastSuccessfulBuild/artifact/src/europlan2.0/Setup/bin/Release/en-US/europlan30.msi"
+	$jenkinsUrlExeDe = $jenkinsUrl + "view/Alle/job/" + $jenkinsJob + "/lastSuccessfulBuild/artifact/src/europlan2.0/Setup/bin/Release/latest_setup/europlan30-de.exe"
+	$jenkinsUrlExeEn = $jenkinsUrl + "view/Alle/job/" + $jenkinsJob + "/lastSuccessfulBuild/artifact/src/europlan2.0/Setup/bin/Release/latest_setup/europlan30-en.exe"
+
 	mkdir $versionDir
-	$newDe = $versionDir + "\europlan30-de.msi";
-	$newEn = $versionDir + "\europlan30-en.msi";
-	copy \\eos\e$\Development\ccnet\europlan2.0_grafisch\src\europlan2.0\Setup\bin\Release\de-DE\europlan30.msi $newDe
-	copy \\eos\e$\Development\ccnet\europlan2.0_grafisch\src\europlan2.0\Setup\bin\Release\en-US\europlan30.msi $newEn
-	copy \\eos\e$\Development\ccnet\europlan2.0_grafisch\src\europlan2.0\Setup\bin\Release\latest_setup\europlan30-de.exe $versionDir
-	copy \\eos\e$\Development\ccnet\europlan2.0_grafisch\src\europlan2.0\Setup\bin\Release\latest_setup\europlan30-en.exe $versionDir
+	$msiDe = $versionDir + "\europlan30-de.msi";
+	$msiEn = $versionDir + "\europlan30-en.msi";
+	$exeDe = $versionDir + "\europlan30-de.exe";
+	$exeEn = $versionDir + "\europlan30-en.exe";
+
+	# download files
+	$wc = New-Object System.Net.WebClient
+	$wc.DownloadFile($jenkinsUrlMsiDe, $msiDe)
+	$wc.DownloadFile($jenkinsUrlMsiEn, $msiEn)
+	$wc.DownloadFile($jenkinsUrlExeDe, $exeDe)
+	$wc.DownloadFile($jenkinsUrlExeEn, $exeEn)
 } else {
 	[System.Windows.Forms.MessageBox]::Show("Das Verzeichnis für Version " + $version + " existiert bereits. Die Dateien wurden nicht kopiert", "Fehler", [System.Windows.Forms.MessageBoxButtons]::Ok, [System.Windows.Forms.MessageBoxIcon]::Error)
 }
