0b37b5279dd7bf29eac60a1e9e6a1c9d3373769e
unknown <kunber@sombrero.bluesource.local>
2015-11-12 14:41:23 +0100

    try replacing CTRL with STRG

--- src/europlan2.0/convertresxtogerman.ps1
+++ src/europlan2.0/convertresxtogerman.ps1
@@ -1,3 +1,3 @@
 Get-ChildItem -Filter *.resx -Recurse |
      ForEach-Object { (Get-Content $_.FullName) -replace
-     "CTRL","STRG" | Set-Content -path $_.FullName }
+     "CTRL","Strg" | Set-Content -path $_.FullName }
