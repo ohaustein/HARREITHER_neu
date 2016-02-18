bd24587f2252e2725a00756c688179a20e3915ba
unknown <kunber@sombrero.bluesource.local>
2016-02-18 11:44:09 +0100

    update release script

--- src/europlan2.0/copy_new_versions.ps1
+++ src/europlan2.0/copy_new_versions.ps1
@@ -1,23 +1,23 @@
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 
 $jenkinsUrl = "http://jenkins:8080/"
-$jenkinsJob = "europlan_grafisch_trunk"
+$jenkinsJob = "europlan_grafisch_release"
 $versionRootDir = "\\fsh\Daten\Share\Software\europlan\versions_ga\"
 
 $version = Get-Content .\curversion.txt
 $versionDir = $versionRootDir + "\v" + $version
 
 if (! ([IO.Directory]::Exists($versionDir))) {
-	$jenkinsUrlMsiDe = $jenkinsUrl + "view/Alle/job/" + $jenkinsJob + "/lastSuccessfulBuild/artifact/src/europlan2.0/Setup/bin/Release/de-DE/europlan30.msi"
-	$jenkinsUrlMsiEn = $jenkinsUrl + "view/Alle/job/" + $jenkinsJob + "/lastSuccessfulBuild/artifact/src/europlan2.0/Setup/bin/Release/en-US/europlan30.msi"
-	$jenkinsUrlExeDe = $jenkinsUrl + "view/Alle/job/" + $jenkinsJob + "/lastSuccessfulBuild/artifact/src/europlan2.0/Setup/bin/Release/latest_setup/europlan30-de.exe"
-	$jenkinsUrlExeEn = $jenkinsUrl + "view/Alle/job/" + $jenkinsJob + "/lastSuccessfulBuild/artifact/src/europlan2.0/Setup/bin/Release/latest_setup/europlan30-en.exe"
+	$jenkinsUrlMsiDe = $jenkinsUrl + "view/Alle/job/" + $jenkinsJob + "/lastSuccessfulBuild/artifact/src/europlan2.0/Setup/bin/Release/de-DE/europlan31.msi"
+	$jenkinsUrlMsiEn = $jenkinsUrl + "view/Alle/job/" + $jenkinsJob + "/lastSuccessfulBuild/artifact/src/europlan2.0/Setup/bin/Release/en-US/europlan31.msi"
+	$jenkinsUrlExeDe = $jenkinsUrl + "view/Alle/job/" + $jenkinsJob + "/lastSuccessfulBuild/artifact/src/europlan2.0/Setup/bin/Release/latest_setup/europlan31-de.exe"
+	$jenkinsUrlExeEn = $jenkinsUrl + "view/Alle/job/" + $jenkinsJob + "/lastSuccessfulBuild/artifact/src/europlan2.0/Setup/bin/Release/latest_setup/europlan31-en.exe"
 
 	mkdir $versionDir
-	$msiDe = $versionDir + "\europlan30-de.msi";
-	$msiEn = $versionDir + "\europlan30-en.msi";
-	$exeDe = $versionDir + "\europlan30-de.exe";
-	$exeEn = $versionDir + "\europlan30-en.exe";
+	$msiDe = $versionDir + "\europlan31-de.msi";
+	$msiEn = $versionDir + "\europlan31-en.msi";
+	$exeDe = $versionDir + "\europlan31-de.exe";
+	$exeEn = $versionDir + "\europlan31-en.exe";
 
 	# download files
 	$wc = New-Object System.Net.WebClient
