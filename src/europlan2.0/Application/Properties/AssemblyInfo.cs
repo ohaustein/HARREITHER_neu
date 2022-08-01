9a19b7dfb204f30cbcd6813208a9b0748ab43176
Oliver Haustein <ohaustein@gmx.de>
2022-08-01 23:09:11 +0200

    Veröffentlichung

--- src/europlan2.0/Application/Properties/AssemblyInfo.cs
+++ src/europlan2.0/Application/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.7.1")]
-[assembly: AssemblyFileVersion("3.1.7.1")]
+[assembly: AssemblyVersion("3.1.8.0")]
+[assembly: AssemblyFileVersion("3.1.8.0")]
