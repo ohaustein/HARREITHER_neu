d0363732dafe10e76be6a7ffd52c3efa62af39d5
Oliver Haustein <ohaustein@gmx.de>
2024-08-10 17:45:36 +0200

    Anpassung des Setups

--- src/europlan2.0/Application/Properties/AssemblyInfo.cs
+++ src/europlan2.0/Application/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.9.1")]
-[assembly: AssemblyFileVersion("3.1.9.1")]
+[assembly: AssemblyVersion("3.2.1.1")]
+[assembly: AssemblyFileVersion("3.2.1.1")]
